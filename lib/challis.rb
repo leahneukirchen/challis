5d31486f3bd943b2f9d3eab8665f1fa14ec67354
Christian Neukirchen <chneukirchen@gmail.com>
2010-02-16 22:24:05 +0100

    Fixes for Ruby 1.9

--- lib/challis.rb
+++ lib/challis.rb
@@ -1,13 +1,13 @@
 # = Challis: a soft lightweight cloth (usually printed)
 #
-# Copyright (C) 2009 Christian Neukirchen <purl.org/net/chneukirchen>
+# Copyright (C) 2009, 2010 Christian Neukirchen <purl.org/net/chneukirchen>
 #
 # Challis is freely distributable under the terms of an MIT-style license.
 # See README or <http://www.opensource.org/licenses/mit-license.php>.
 
 class Challis < String
   def pfmt(t)
-    t.gsub(/\\([^\\\n]|\\(?!\n))/) { "&ChallisEscape#{$&[1]};" }.
+    t.gsub(/\\([^\\\n]|\\(?!\n))/) { "&ChallisEscape#{$&.unpack("U*")[1]};" }.
       gsub(/&(?!#\d+;|#x[\da-fA-F]+;|\w+;)/, "&amp;"). # keep entities
       gsub("<", "&lt;").
       gsub(">", "&gt;").
@@ -21,7 +21,7 @@ class Challis < String
       gsub(/\[(\S+)\]/m,              '<a href="\1">\1</a>').
       gsub(/\[(.*?)\s?(\S+)\]/m,      '<a href="\2">\1</a>').
       gsub(/\{([\w:.-]+) (.*?)\}/m,   '<span class="\1">\2</span>').
-      gsub(/&ChallisEscape(\d+);/) { $1.to_i.chr }.
+      gsub(/&ChallisEscape(\d+);/) { [$1.to_i].pack("U*") }.
       strip
   end
   
@@ -39,11 +39,11 @@ class Challis < String
 
     (split(/\n\n+|(?=^(?:  )*(?:\* |# |=+ ))|^(---.*?^---)|^(\{\{[\w:. -]+)|^(\}\})/m).map { |par|
        case par
-       when /\A---html\n(.*)^---/m: $1
-       when /\A---\n(.*)^---/m:     %Q{<pre><code>#{prefmt $1}</code></pre>}
-       when /\A\{\{([\w:. -]+)\z/:  %Q{<div class="#{$1}">}
-       when "}}":                   %Q{</div>}
-       when /\A\s*\z/:              nil  # ignore
+       when /\A---html\n(.*)^---/m; $1
+       when /\A---\n(.*)^---/m;     %Q{<pre><code>#{prefmt $1}</code></pre>}
+       when /\A\{\{([\w:. -]+)\z/;  %Q{<div class="#{$1}">}
+       when "}}";                   %Q{</div>}
+       when /\A\s*\z/;              nil  # ignore
        when /\A((?:  )*)((?:\* |# |" |.*::(?: |$)|=+ )?)(.*)/m  #/
          indent, type, text = $1, $2.strip, $3
 
@@ -72,27 +72,27 @@ class Challis < String
 
          if fresh
            case type
-           when '*':  to_close.unshift %Q{</li></ul>}
-           when '#':  to_close.unshift %Q{</li></ol>}
-           when '"':  to_close.unshift %Q{</blockquote>}
-           when '::': to_close.unshift %Q{</dd></dl>}
+           when '*';  to_close.unshift %Q{</li></ul>}
+           when '#';  to_close.unshift %Q{</li></ol>}
+           when '"';  to_close.unshift %Q{</blockquote>}
+           when '::'; to_close.unshift %Q{</dd></dl>}
            end
          end
 
          case type
-         when '*':        text = "<li>" + text
-         when '#':        text = "<li>" + text
-         when /\A(=+)\z/: text = "<h#{$1.size}>#{text}</h#{$1.size}>"
-         when '::':       text = (dt.empty? ? "" : "<dt>#{dt}</dt>") + "<dd>" + text
+         when '*';        text = "<li>" + text
+         when '#';        text = "<li>" + text
+         when /\A(=+)\z/; text = "<h#{$1.size}>#{text}</h#{$1.size}>"
+         when '::';       text = (dt.empty? ? "" : "<dt>#{dt}</dt>") + "<dd>" + text
          end
          
          text.gsub!(/\A<(\w+)>/, %Q{<\\1 id="#{id}">})  if id
 
          case type
-         when '*':  text = (fresh ? "<ul>"         : "</li>") + text
-         when '#':  text = (fresh ? "<ol>"         : "</li>") + text
-         when '"':  text = (fresh ? "<blockquote>" : ""     ) + text
-         when '::': text = (fresh ? "<dl>"         : "</dd>") + text
+         when '*';  text = (fresh ? "<ul>"         : "</li>") + text
+         when '#';  text = (fresh ? "<ol>"         : "</li>") + text
+         when '"';  text = (fresh ? "<blockquote>" : ""     ) + text
+         when '::'; text = (fresh ? "<dl>"         : "</dd>") + text
          end
 
          depth = new_depth
